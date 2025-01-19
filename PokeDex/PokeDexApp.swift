3aae4f1e521b96aaf48b9dbf4e8cc70ce3e065da
sweetclimusic <4057640+sweetclimusic@users.noreply.github.com>
2025-01-19 23:11:28 +0000

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Ashlee Muscroft on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
